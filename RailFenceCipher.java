0253efe2bc730ae6b87f0ec9a9729a612c90c673
Yung Tseng <30759867+yao-tseng@users.noreply.github.com>
2017-09-08 14:46:40 -0400

    Update RailFenceCipher.java

--- RailFenceCipher.java
+++ RailFenceCipher.java
@@ -4,27 +4,30 @@ import java.io.*;
 public class RailFenceCipher {
 
 	public static void main( String[] args ) {
-		String input = "";
-		String plaintxt = "";
+		//create variables;
+		//(String)input: user input;
+		//(String)plaintxt: user input in uppercase without spaces;
+		//(Integer)numOfRails: user set number of rails;
+		String input = "to infinity and beyond";
+		String plaintxt = "TOINFINITYANDBEYOND";
 		int numOfRails = 3;
 		
-		if ( args.length == 0 ) {
-			System.out.println( "empty input, execute default..." );
-			input = "to infinity and beyond";
-			plaintxt = "TOINFINITYANDBEYOND";
-		}
+		//user input is empty, use default values;
+		if ( args.length == 0 ) System.out.println( "empty input, execute default..." );
 		
+		//if user input is a single number, set to be the number of rail;
+		//if user input is a single word, set to be the plaintext;
 		else if ( args.length == 1 ) {
-			try {
-				numOfRails = Integer.parseInt( args[0] );
-				input = "to infinity and beyond";
-				plaintxt = "TOINFINITYANDBEYOND";
+			try { numOfRails = Integer.parseInt( args[0] );
 			} catch ( NumberFormatException nfe ) {
 				input = args[0];
 				plaintxt = args[0];
 			}
 		}
 
+		//if user inputs more than one agrument, take the last argument as the number of rail and all
+		//others as plaintext;
+		//if the last argument input is not a number, concatenate the last argument to plaintext;
 		else {
 			try {
 				for ( int i=0; i<args.length-1; i++ ) {
@@ -37,31 +40,43 @@ public class RailFenceCipher {
 				plaintxt += args[args.length-1];
 			}
 		}
+		//plaintext should be all in uppercase;
 		plaintxt = plaintxt.toUpperCase();
 		
-		System.out.println( "Input     : " + input + '\n' + "Plaintext : " + plaintxt + '\n' + "# of rails: " + numOfRails );
-		System.out.println();
+		//print input data;
+		System.out.println( "Input     : " + input + '\n' + "# of rails: " + numOfRails );
 
-		encrypt( numOfRails, plaintxt );
+		//(Array)codeArr: an array which stores ciphertext;
+		//call function "encrypt";
+		char[] codeArr = encrypt( numOfRails, plaintxt );
+		System.out.print( "Plaintext : " + plaintxt + '\n' + "Ciphertext: " );
+		//print ciphertext;
+		for ( int i=0; i<plaintxt.length(); i++ ) System.out.print(codeArr[i]);
+		System.out.println();
 	}
 
-	public static void encrypt( int numOfRails, String plaintxt ) {
-		int index = 0;
+	//(Function)encrypt: encrypt plaintext;
+	public static char[] encrypt( int numOfRails, String plaintxt ) {
+		//(Integer)indexPt: index of letters in plaintext;
+		//(Integer)railCnt: rail count;
+		//(Array)arr: array for storing ciphertext;
+		//(Integer)indexCt: index of letters in ciphertext;
+		int indexPt = 0;
 		int railCnt = 0;
 		char[] arr = new char[plaintxt.length()];
-		int a = 0;
+		int indexCt = 0;
 
 		while( railCnt<numOfRails ) {		
-			for ( index=0; index<plaintxt.length(); index++ ) {
-				int math = index % ((numOfRails-1)*2);
-				if ( math == railCnt || math == ((numOfRails-1) * 2 - railCnt) ) {
-					arr[a] = plaintxt.charAt(index);
-					a = a + 1;
+			for ( indexPt=0; indexPt<plaintxt.length(); indexPt++ ) {
+				int math = indexPt % ( (numOfRails-1) * 2 );
+				if ( math == railCnt || math == ( (numOfRails-1) * 2 - railCnt) ) {
+					arr[indexCt] = plaintxt.charAt(indexPt);
+					indexCt = indexCt + 1;
 				}
 			}
 			railCnt = railCnt + 1;
 		}
-		for ( int i=0; i<plaintxt.length(); i++ ) System.out.print( arr[i] );
-		System.out.println();
+
+		return arr;
 	}
 }
